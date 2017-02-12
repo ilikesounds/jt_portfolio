dd63a92db2fe5badaaa6ce4559620e8ad4e4be67
Jeff Torres <jeffrey.n.torres@gmail.com>
2017-02-11 17:07:02 -0800

    close #1, close #2, close #3, close #4

---
+++ jt_portfolio/settings/dev.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Django development settings for jt_portfolio project.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.10/topics/settings/
+
+For the full list of settings and their values, see
+https://docs.djangoproject.com/en/1.10/ref/settings/
+"""
+
+from base import *
