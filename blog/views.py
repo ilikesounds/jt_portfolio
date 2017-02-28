ce46009d1d4f6b89efda11ab754ec72c80a38d2d
Jeff Torres <jeffrey.n.torres@gmail.com>
2017-02-27 16:58:29 -0800

    init blog object created and migrated, closes #10 #11 #12

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def home_view(request):
+    """home view test"""
+
+    context = {
+        'page_title': 'HOME',
+    }
+    return render(request, 'blog/home.html', context)
