883dde8de1a39af270a306a7c02cbc96408531c5
Jeff Torres <jeffrey.n.torres@gmail.com>
2017-03-14 21:32:25 -0700

    moved static dir for heroku deployment

--- jt_portfolio/settings.py
+++ jt_portfolio/settings.py
@@ -136,6 +136,7 @@ ALLOWED_HOSTS = ['*']
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+print(STATIC_ROOT)
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
