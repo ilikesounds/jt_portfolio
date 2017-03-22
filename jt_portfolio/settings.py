4ecd358e8703d9fabcf67f7886ede6819cc0f89e
Jeff Torres <jeffrey.n.torres@gmail.com>
2017-03-21 19:57:00 -0700

    added new settings structure for eventual move from heroku

--- jt_portfolio/settings.py
+++ jt_portfolio/settings.py
@@ -87,6 +87,16 @@ if not local:
         }
     }
 
+    EMAIL_USE_TLS = True
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST_USER = os.environ['EMAIL_USER']
+    EMAIL_HOST_PASSWORD = os.environ['EMAIL_PW']
+    EMAIL_PORT = 587
+    DEFAULT_FROM_EMAIL = os.environ['EMAIL_DEFAULT']
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    ACCOUNT_ACTIVATION_DAYS = 3
+
+
 else:
     DATABASES = {
         'default': {
@@ -98,6 +108,17 @@ else:
             'PORT': os.environ['DB_PORT'],
         }
     }
+
+    # Email credentials
+    EMAIL_USE_TLS = True
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST_USER = os.environ['EMAIL_USER']
+    EMAIL_HOST_PASSWORD = os.environ['EMAIL_PW']
+    EMAIL_PORT = 587
+    DEFAULT_FROM_EMAIL = os.environ['EMAIL_DEFAULT']
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    ACCOUNT_ACTIVATION_DAYS = 3
+
 
 
 # Password validation
