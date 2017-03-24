f017b1d7ce835afe22bd6cd9f56108c7baea84d2
Jeff Torres <jeffrey.n.torres@gmail.com>
2017-03-24 13:10:05 -0700

    split settings file into base, dev, prod

---
+++ jt_portfolio/settings/dev.py
@@ -0,0 +1,19 @@
+import os
+from base import *
+
+
+DEBUG = True
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ['DB_NAME'],
+        'USER': os.environ['DB_USER'],
+        'PASSWORD': os.environ['DB_PWD'],
+        'HOST': os.environ['DB_HOST'],
+        'PORT': os.environ['DB_PORT'],
+    }
+}
+
+# Email credentials
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
---
+++ jt_portfolio/settings/prod.py
@@ -0,0 +1,33 @@
+import os
+import dj_database_url
+from base import *
+
+
+DEBUG = False
+
+DATABASES = {
+    'default': {
+        'ENGINE': dj_database_url.config(
+            default=os.environ.get('DATABASE_URL')
+            )
+        }
+    }
+
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = os.environ['EMAIL_USER']
+EMAIL_HOST_PASSWORD = os.environ['EMAIL_PW']
+EMAIL_PORT = 587
+DEFAULT_FROM_EMAIL = os.environ['EMAIL_DEFAULT']
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+ACCOUNT_ACTIVATION_DAYS = 3
+
+# Update database configuration with $DATABASE_URL.
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
