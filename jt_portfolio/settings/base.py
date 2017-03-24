dd66e764611733abca6d7d1af0f0fd72eba7639c
Jeff Torres <jeffrey.n.torres@gmail.com>
2017-03-24 13:27:32 -0700

    changed BASE_DIR to PROJECT_ROOT

--- jt_portfolio/settings/base.py
+++ jt_portfolio/settings/base.py
@@ -8,8 +8,8 @@ import os
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'jt_portfolio.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(PROJECT_ROOT, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
