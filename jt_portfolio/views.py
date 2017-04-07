4b6a92576daff6b88b3060b86b8889d011ea74e1
Jeff Torres <jeffrey.n.torres@gmail.com>
2017-04-06 19:11:34 -0700

    added get_context_data override to home view

--- jt_portfolio/views.py
+++ jt_portfolio/views.py
@@ -1,6 +1,17 @@
 from django.views.generic import TemplateView
+from jt_portfolio.about_me.models import Skills
 
 
 class Home(TemplateView):
 
     template_name = 'jt_portfolio/home.html'
+
+    def get_contxt_data(self, **kwargs):
+        """
+        Override default context data for TemplateView
+        """
+
+        context = super(TemplateView, self).get_contxt_data(**kwargs)
+        skills_queryset = self.Skills.object.all()
+        context['skills'] = skills_queryset
+        return context
